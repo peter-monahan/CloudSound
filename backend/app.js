348470114574aea881d460fcc1f440c43e7d5796
peter <peter.b.monahan@gmail.com>
2022-07-02 15:28:06 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-//-------------------------------------------------- Requiring Node Modules
+//--------------------------------------------------
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
@@ -6,9 +6,11 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-//-------------------------------------------------- Getting isProduction Bool
+
 const {environment} = require('./config');
 const isProduction = (environment === 'production');
+
+const { ValidationError } = require('sequelize');
 //-------------------------------------------------- Initializing express and adding middleware
 const app = express();
 
@@ -39,9 +41,38 @@ app.use(
     }
   })
 );
-//-------------------------------------------------- Requiring route handlers
-const routes = require('./routes');
 //--------------------------------------------------
+const routes = require('./routes');
+
 app.use(routes);
+
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  // check if error is a Sequelize error
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+   title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
