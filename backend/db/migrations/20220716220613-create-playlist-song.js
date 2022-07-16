7c8aa5bfba314c64fd799c306c49fb1b2eb81f75
peter <peter.b.monahan@gmail.com>
2022-07-16 17:10:13 -0500

    added playlistsong mig and mod

---
+++ backend/db/migrations/20220716220613-create-playlist-song.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('PlaylistSongs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      songId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Songs',
+          key: 'id',
+        },
+        onDelete: 'CASCADE'
+      },
+      playlistId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Playlists',
+          key: 'id',
+        },
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('PlaylistSongs');
+  }
+};
